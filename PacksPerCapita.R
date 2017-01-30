7d5404f372c5b28d3e7c857ec535f3721189bab4
HighTechnocracy <jim@hightechnocracy.com>
2017-01-30 12:40:44 -0700

    Several edits to R code

--- PacksPerCapita.R
+++ PacksPerCapita.R
@@ -1,10 +1,12 @@
+setwd("~/R/PacksPerCapita")
+
 library(ggplot2)
-data <- read.csv("PPC_data.csv")
+data <- read.csv("~/R/PacksPerCapita/Data/PPC_data.csv")
 
 data$YEAR <- as.character(data$YEAR)
 data$YEAR <- as.Date(data$YEAR, "%Y")
 
-pdf(file = "Rplots.pdf")
+pdf(file = "~/R/PacksPerCapita/Visuals/ppc.pdf")
 ggplot(data, aes(data$YEAR, data$PPC)) + geom_line() + 
         xlab("calendar year") + ylab("packs per year") +
         stat_smooth(colour="green")
