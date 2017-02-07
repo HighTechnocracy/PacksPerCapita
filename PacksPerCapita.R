bf56c1700dbfadcbb3ee35c10125960c43e23378
HighTechnocracy <jim@hightechnocracy.com>
2017-02-07 13:19:35 -0700

    Added theme elements and a chart title

--- PacksPerCapita.R
+++ PacksPerCapita.R
@@ -7,8 +7,11 @@ data$YEAR <- as.character(data$YEAR)
 data$YEAR <- as.Date(data$YEAR, "%Y")
 
 pdf(file = "~/R/PacksPerCapita/Visuals/ppc.pdf")
-ggplot(data, aes(data$YEAR, data$PPC)) + geom_line() + 
-        xlab("calendar year") + ylab("packs per year") +
-        stat_smooth(colour="blue")
-
+ggplot(data, aes(data$YEAR, data$PPC)) + 
+        geom_line(alpha = 0.6) + 
+        xlab("Year") + ylab("Packs") +
+        geom_smooth(colour="blue") +
+        ggtitle("Cigarettes Purchased in Colorado per Capita, 1990-2016") +
+        theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=20, hjust=0)) +
+        theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=15))
 dev.off()
